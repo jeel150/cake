43357224940d2090fe88607c712ed94e4feb931d
jeel150 <shahjeel865@gmail.com>
2025-09-22 11:12:26 +0530

    Fix start script for Render (use server.js)

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
 import cors from 'cors';
-import connectDB from './config/db.js';
+import connectDB from './Config/db.js';
 import productRoutes from './Routes/productRoutes.js';
 import authRoutes from "./Routes/authRoutes.js";
 import userRoutes from "./Routes/userRoutes.js";
